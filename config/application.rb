9722726af5d92048949955b774283761f3c263b9
phunt <phunt@runsystem.net>
2020-03-18 17:43:20 +0700

    Scaffold User

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    # config.active_record.writing_role = :default
+    # config.active_record.reading_role = :readonly
+
+    config.active_record.database_selector = { delay: 2.seconds }
+    config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
+    config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   end
 end
